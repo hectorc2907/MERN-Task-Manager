8111a795a3fd6a5eea699b675d05e017c4ed0a76
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-16 13:35:15 -0300

    generamos el formulario de registro y probamos su funcionamiento

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { useState } from "react";
+import { post } from "../services/authServices.js";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-export default Register
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const request = await post("/api/auth/register", {
+        name,
+        email,
+        password,
+      });
+      if (request.status === 200) {
+        toast.success("Registro Exitoso");
+        navigate("/login");
+      }
+    } catch (error) {
+      toast.error("Registro Fallido");
+    }
+  };
+  return (
+    <>
+      <div>
+        <h2>Registro</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="name"
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Registrar</button>
+          <p className="register-link">
+            Already have an account? <Link to="/login">Login here</Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
