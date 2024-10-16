ef255bd99abf79fa9fec795694cdce259777f1c2
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-16 09:37:58 -0300

    agregamos la ruta del logout y verificamos su funcionamiento

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { userLogin, userRegister } from "../controllers/authControllers.js";
+import {
+  userLogin,
+  userLogout,
+  userRegister,
+} from "../controllers/authControllers.js";
 
 //configuramos el enrutador
 const router = express.Router();
@@ -7,6 +11,7 @@ const router = express.Router();
 //definimos las rutas
 router.post("/register", userRegister);
 router.post("/login", userLogin);
+router.post("/logout", userLogout);
 
 //exportamos el enrutador
 export default router;
