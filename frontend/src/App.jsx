f2b65171dba06033c426f04c45407db221668437
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-16 11:32:49 -0300

    configuramos las rutas en app.jsx para que se pueda acceder segun su login y role

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { updateUser } from "./redux/AuthSlice.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import PublicLayouts from "./layouts/PublicLayouts";
+import UserLayouts from "./layouts/UserLayouts";
+import AdminLayouts from "./layouts/AdminLayouts";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const user = useSelector((state) => state.Auth.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(updateUser());
+  }, [dispatch]);
+  return (
+    <Router>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<PublicLayouts />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route path="/" element={<UserLayouts />}>
+          <Route index element={<Home />} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayouts />}>
+          <Route index element={<Admin />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
